981d8ef395e244b47fece2b9244a28fadd6f602a
Reimjel Senas <reimjel.senas@iist.edu.ph>
2025-12-19 15:35:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener('gesturestart', function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener('dblclick', function (e) {
-  e.preventDefault();
-});
-
-
-
 // ======================
 // RIASEC QUESTIONS
 // ======================
